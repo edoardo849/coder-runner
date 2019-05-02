e9a3e56c19d1c600608a7271b709a03266f1cbcc
Edo Scalafiotti <edoardo849@gmail.com>
2019-05-02 12:37:30 +0000

    refactoring

--- install.sh
+++ install.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 
-install_l=false 
-while getopts v:ld: option
-do
+install_l=false
+while getopts v:ld: option; do
 	case "${option}" in
 
-	v)	
+	v)
 		echo "Installing version $OPTARG"
 		version=${OPTARG}
 		;;
-	l) 
+	l)
 		echo "Installing letsencrypt"
 		install_l=true
 		;;
-	d) domain=${OPTARG};;
+	d) domain=${OPTARG} ;;
 	esac
 done
 
 confDir=$HOME/.config
 if [ ! -d "$confDir" ]; then
-        echo "- Creating $confDir"
-        mkdir $confDir
+	echo "- Creating $confDir"
+	mkdir $confDir
 fi
 
 if [ ! -f $confDir/coder.conf ]; then
-    cp ./example_coder.conf $confDir/coder.conf
+	cp ./example_coder.conf $confDir/coder.conf
 fi
 
 binDir=$HOME/bin
@@ -40,45 +39,44 @@ version=$(curl -L --silent "https://api.github.com/repos/$repo/releases/latest" 
 echo "The latest release is $version"
 
 # if the version variable is empty, exit
-if [ -z "$version" ]; then 
+if [ -z "$version" ]; then
 	echo "Could not retrieve the latest version"
 	exit 1
 fi
 
 if [ -f $confDir/coder_latest.txt ]; then
-	installed_version=$(cat "$confDir/coder_latest.txt" )
+	installed_version=$(cat "$confDir/coder_latest.txt")
 	if [ "$version" == "$installed_version" ]; then
 		echo "The latest version of coder is already installed"
 		do_install=false
-	else 
+	else
 		echo "Latest version is $version , installed is $installed_version , upgrading..."
 	fi
 fi
 
-if [ "$do_install" == true ]; then 
+if [ "$do_install" == true ]; then
 	echo "Installing version $version"
-	wget "https://github.com/codercom/code-server/releases/download/$version/code-server$version-linux-x64.tar.gz" && \
-	tar -xvzf code-server$version-linux-x64.tar.gz && \
-	chmod +x ./code-server$version-linux-x64/code-server && \
-	mv ./code-server$version-linux-x64/code-server $binDir/code-server
-	
+	wget "https://github.com/codercom/code-server/releases/download/$version/code-server$version-linux-x64.tar.gz" &&
+		tar -xvzf code-server$version-linux-x64.tar.gz &&
+		chmod +x ./code-server$version-linux-x64/code-server &&
+		mv ./code-server$version-linux-x64/code-server $binDir/code-server
+
 	echo "Cleaning up"
-	rm code-server$version-linux-x64.tar.gz && \
+	rm code-server$version-linux-x64.tar.gz &&
 		rm -rf code-server$version-linux-x64
-	
-	echo "$version" > $confDir/coder_latest.txt
+
+	echo "$version" >$confDir/coder_latest.txt
 fi
 
 # if the -l flag is not empty, install letsencrypt
 if [ "$install_l" == true ]; then
 
-	if [ -z "$domain" ]; then 
+	if [ -z "$domain" ]; then
 		echo "The domain -d flag must be set"
 		exit
 	fi
 	echo "Installing certbot COMING SOON"
-	
+
 fi
 
 echo "Done."
-
